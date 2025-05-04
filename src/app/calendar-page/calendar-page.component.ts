c5f853be7b15af2848d1423486c76a0ea3dbf134
01szak <kacperolszewski2006@gmail.com>
2025-05-04 11:55:48 +0200

    created event emitter for date picker to calendar-page

--- src/app/calendar-page/calendar-page.component.ts
+++ src/app/calendar-page/calendar-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {MatCard} from '@angular/material/card';
 import {CalendarComponent} from './calendar/calendar.component';
 import {ReservationPanelComponent} from './reservation-panel/reservation-panel.component';
@@ -17,5 +17,18 @@ import {DatePickerComponent} from '../date-picker/date-picker.component';
   styleUrl: './calendar-page.component.css'
 })
 export class CalendarPageComponent {
+  months: string[] = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+
+  @Input() month: string = this.months[new Date().getMonth()];
+  @Input() year: number = new Date().getFullYear();
+
+  changeMonth(event:string) {
+  console.log(event)
+    this.month = event;
+  }
+  changeYear(event:number) {
+    console.log(event)
+    this.year = event;
+  }
 
 }
--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-date-picker',
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
   standalone: true,
   styleUrl: './date-picker.component.css'
 })
-export class DatePickerComponent {
+export class DatePickerComponent implements OnInit {
+  months: string[] = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+  currentMonth: string = this.months[new Date().getMonth()];
+  @Output() month = new EventEmitter<string>();
+  @Output() year= new EventEmitter<number>();
+  yearForTemplate: number = new Date().getFullYear();
+  monthForTemplate: string = this.months[new Date().getMonth()]
 
+  ngOnInit(): void {
+
+  }
+
+
+  decreaseMonth() {
+    let index = this.months.indexOf(this.currentMonth);
+    if (index < 1) {
+    this.yearForTemplate --;
+      this.year.emit(this.yearForTemplate)
+      index = this.months.length;
+    }
+    let month = this.months[index - 1];
+    this.currentMonth = month
+    this.monthForTemplate = month;
+    this.month.emit(month)
+  }
+
+  increaseMonth() {
+    let index = this.months.indexOf(this.currentMonth);
+    if (index >= this.months.length - 1) {
+      this.yearForTemplate ++;
+      this.year.emit(this.yearForTemplate)
+      index = -1
+    }
+    let month = this.months[index + 1];
+    this.currentMonth = month;
+    this.monthForTemplate = month;
+    this.month.emit(month);
+  }
 }
